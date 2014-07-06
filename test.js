d81771eff1c45c6045db0f302874bdf78e874c27
wilhelmson <j.williamson.sd@gmail.com>
2014-07-05 20:03:59 -0700

    byDay byWeek byMonth need to return

--- test.js
+++ test.js
@@ -62,3 +62,62 @@ test('Test Months', function (t) {
   t.end()
 
 })
+test('Test byDays', function (t) {
+
+  var days = groupByTime.byDay(data, 'ts');
+
+  t.equal(
+    _.keys(days).length
+  , 196
+  , 'Has the proper number of days'
+  )
+
+  t.equal(
+    _.flatten(_.toArray(days)).length
+  , data.length
+  , 'The length of all combined days equals the original data length'
+  )
+
+  t.end()
+
+})
+
+test('Test byWeeks', function (t) {
+
+  var weeks = groupByTime.byWeek(data, 'ts');
+
+  t.equal(
+    _.keys(weeks).length
+  , 45
+  , 'Has the proper number of weeks'
+  )
+
+  t.equal(
+    _.flatten(_.toArray(weeks)).length
+  , data.length
+  , 'The length of all combined weeks equals the original data length'
+  )
+
+  t.end()
+
+})
+
+test('Test byMonths', function (t) {
+
+  var months = groupByTime.byMonth(data, 'ts');
+
+  t.equal(
+    _.keys(months).length
+  , 14
+  , 'Has the proper number of months'
+  )
+
+  t.equal(
+    _.flatten(_.toArray(months)).length
+  , data.length
+  , 'The length of all combined months equals the original data length'
+  )
+
+  t.end()
+
+})
